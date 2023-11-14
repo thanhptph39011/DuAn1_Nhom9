5d1b3d67f8a7d99d91a802be486205fa98c6893c
ThanhDev <thanhdeptraivcll204@gmail.com>
2023-11-14 21:21:32 +0700

    Crud cơ sở commit

---
+++ app/src/main/java/com/example/duan1_nhom9/Adapter/CoSoAdapter.java
@@ -0,0 +1,157 @@
+package com.example.duan1_nhom9.Adapter;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.duan1_nhom9.DAO.CoSoDao;
+import com.example.duan1_nhom9.Model.CoSo;
+import com.example.duan1_nhom9.R;
+
+import java.util.ArrayList;
+
+public class CoSoAdapter extends RecyclerView.Adapter<CoSoAdapter.viewholer> {
+    private final Context context;
+    private final ArrayList<CoSo> listcs;
+    CoSoDao coSoDao;
+
+    public CoSoAdapter(Context context, ArrayList<CoSo> listcs) {
+        this.context = context;
+        this.listcs = listcs;
+        coSoDao = new CoSoDao(context);
+    }
+
+    @NonNull
+    @Override
+    public viewholer onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+        View view = inflater.inflate(R.layout.item_coso, null);
+        return new viewholer(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewholer holder, int position) {
+        holder.maCoSo.setText(listcs.get(position).getMaCoSo());
+        holder.diaChi.setText(listcs.get(position).getDiaChi());
+        CoSo cs = listcs.get(position);
+        holder.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Cảnh báo");//set tiêu đề
+                builder.setIcon(R.drawable.baseline_warning_24);//set icon
+                builder.setMessage("Bạn có chắc chắn muốn xóa không?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (coSoDao.deleteCoSo(cs.getMaCoSo())) {
+                            listcs.clear();
+                            listcs.addAll(coSoDao.GetAll());
+                            notifyDataSetChanged();
+                            Toast.makeText(context, "Delete Succ", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(context, "Huỷ xoá", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+        holder.btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opendialogUpdate(cs);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return listcs.size();
+    }
+
+    public class viewholer extends RecyclerView.ViewHolder {
+        TextView maCoSo, diaChi;
+        ImageView btnDelete, btnUpdate;
+
+        public viewholer(@NonNull View itemView) {
+            super(itemView);
+            maCoSo = itemView.findViewById(R.id.tvMaCs_itemCoSo);
+            diaChi = itemView.findViewById(R.id.tvDiaChi_itemCoSo);
+            btnUpdate = itemView.findViewById(R.id.btnUpdate_CoSo);
+            btnDelete = itemView.findViewById(R.id.btnDelete_CoSo);
+        }
+    }
+
+    public void opendialogUpdate(CoSo cs) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+//
+        LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+        View view = inflater.inflate(R.layout.item_update_coso, null);
+        builder.setView(view);//gán view vào hôp thoại
+        Dialog dialog = builder.create();//tạo hộp thoại
+        dialog.show();
+        //Ánh xạ
+        EditText maCs = view.findViewById(R.id.edtTenCs_itemUpCs);
+        EditText diaChi = view.findViewById(R.id.edtDiaChi_itemUpCs);
+        ImageView btnSave = view.findViewById(R.id.update_Cs);
+        ImageView btnHuy = view.findViewById(R.id.huyUpdate_Cs);
+        //Gán dl
+        maCs.setText(cs.getMaCoSo());
+        diaChi.setText(cs.getDiaChi());
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tenCs = maCs.getText().toString();
+                String dchi = diaChi.getText().toString();
+                if (tenCs.equals("")) {
+                    Toast.makeText(context, "Vui lòng nhập tên cơ sở", Toast.LENGTH_SHORT).show();
+                    maCs.requestFocus();
+                    return;
+                }
+                if (diaChi.equals("")) {
+                    Toast.makeText(context, "Vui lòng nhập địa chỉ", Toast.LENGTH_SHORT).show();
+                    diaChi.requestFocus();
+                    return;
+                }
+                cs.setMaCoSo(maCs.getText().toString());
+                cs.setDiaChi(diaChi.getText().toString());
+                if (coSoDao.updateCoSo(cs)) {
+                    listcs.clear();
+                    listcs.addAll(coSoDao.GetAll());
+                    notifyDataSetChanged();
+                    dialog.dismiss();
+                    Toast.makeText(context, "Update Succ", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context, "Update Fail", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnHuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "Huỷ Update", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/duan1_nhom9/Fragments/CoSoFragment.java
+++ app/src/main/java/com/example/duan1_nhom9/Fragments/CoSoFragment.java
@@ -1,19 +1,40 @@
 package com.example.duan1_nhom9.Fragments;
 
+import android.app.Dialog;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.duan1_nhom9.Adapter.CoSoAdapter;
+import com.example.duan1_nhom9.DAO.CoSoDao;
+import com.example.duan1_nhom9.Model.CoSo;
 import com.example.duan1_nhom9.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 
 public class CoSoFragment extends Fragment {
 
-
+    RecyclerView rycCoSo;
+    FloatingActionButton fltAddCoSo;
+    EditText edtTimKiem;
+    CoSoDao coSoDao;
+    CoSoAdapter adapter;
+    ArrayList<CoSo> tempList; //khai báo mảng đệm
+    private ArrayList<CoSo> listcs = new ArrayList<CoSo>();
 
     public CoSoFragment() {
         // Required empty public constructor
@@ -24,8 +45,93 @@ public class CoSoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v= inflater.inflate(R.layout.fragment_co_so, container, false);
-        return v;
+        View v = inflater.inflate(R.layout.fragment_co_so, container, false);
+        rycCoSo = v.findViewById(R.id.rycCoSo);
+        fltAddCoSo = v.findViewById(R.id.fltAddCoSo);
+        edtTimKiem = v.findViewById(R.id.edtTimKiem);
+        coSoDao = new CoSoDao(getActivity());
+        listcs = (ArrayList<CoSo>) coSoDao.GetAll();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false);
+        tempList = (ArrayList<CoSo>) coSoDao.GetAll();
+        rycCoSo.setLayoutManager(layoutManager);
+        adapter = new CoSoAdapter(getActivity(), listcs);
+        rycCoSo.setAdapter(adapter);
+        fltAddCoSo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opendialogAdd();
+            }
+        });
+        edtTimKiem.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//code
+                listcs.clear();
+                for (CoSo cs : tempList) {
+                    if (cs.getMaCoSo().contains(charSequence.toString())) {
+                        listcs.add(cs);
+                    }
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        return v;
+    }
+
+    private void opendialogAdd() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.item_add_coso, null);
+        builder.setView(view);
+        Dialog dialog = builder.create();
+        dialog.show();
+        EditText edtTenCs = view.findViewById(R.id.edtTenCs_itemAddCs);
+        EditText edtDiaChi = view.findViewById(R.id.edtDiaChi_itemAddCs);
+        ImageView btnSave = view.findViewById(R.id.Add_Cs);
+        ImageView btnHuy = view.findViewById(R.id.huyAdd_Cs);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tenCs = edtTenCs.getText().toString();
+                String dchi = edtDiaChi.getText().toString();
+                if (tenCs.equals("")) {
+                    Toast.makeText(getActivity(), "Vui lòng nhập tên cơ sở", Toast.LENGTH_SHORT).show();
+                    edtTenCs.requestFocus();
+                    return;
+                }
+                if (dchi.equals("")) {
+                    Toast.makeText(getActivity(), "Vui lòng nhập địa chỉ", Toast.LENGTH_SHORT).show();
+                    edtDiaChi.requestFocus();
+                    return;
+                }
+                CoSo cs = new CoSo(tenCs, dchi);
+                if (coSoDao.insertCoSo(cs)) {
+                    listcs.clear();
+                    listcs.addAll(coSoDao.GetAll());
+                    adapter.notifyDataSetChanged();
+                    Toast.makeText(getActivity(), "Add Succ", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                } else {
+                    Toast.makeText(getActivity(), "Add Fail", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnHuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "Huỷ Add", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
     }
 }
